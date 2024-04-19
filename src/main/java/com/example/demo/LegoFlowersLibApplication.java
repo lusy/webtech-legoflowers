c095886a5062e676d96568fc9fdc21df27f3ad17
lusy <vaseva@mi.fu-berlin.de>
2024-04-19 11:43:08 +0200

    Init project

---
+++ src/main/java/com/example/demo/LegoFlowersLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoFlowersLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
