e800e9985df3ce1df23303beb396050fac7983e1
lusy <vaseva@mi.fu-berlin.de>
2024-06-10 13:28:24 +0200

    Add WebConfig

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,5 +1,23 @@
 package config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:5173/",
+                        "https://webtech-legoflowers-frontend.onrender.com/"
+                );
+    }
 
 }
